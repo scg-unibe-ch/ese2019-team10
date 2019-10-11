6f1004d3f2f325219ebf448419687da3f2bcfd71
David Herrmann <david.herrmann@protonmail.com>
2019-10-12 00:11:00 +0200

    Add some minor improvements to the docker helper-script

--- backend/build_and_run.sh
+++ backend/build_and_run.sh
@@ -7,10 +7,20 @@
 # webserver running phpmyadmin. The database is not      #
 # accessible from outside the docker network.            #
 #                                                        # 
+# The volume for the database container ist stored at    #
+# path specified by the first command line argument.     #
+#                                                        # 
 # phpmyadmin-dev  - localhost:8081                       #
 # backend-dev - localhost:3000                           #
 # database-dev - database-dev:3306 (docker network)      #
 ##########################################################
+
+if [ ! $1 ] ; then
+    echo "Usage: $0 PATH_TO_VOLUME_DIRECTORY"
+    exit
+fi
+
+VOLUME_PATH=$1
 
 # create network for dev, ignor error if it does not already exist
 docker network create dev-net
@@ -22,7 +32,11 @@ docker build --no-cache -t backend-dev ./
 docker stop backend-dev database-dev phpmyadmin-dev
 docker container rm backend-dev database-dev phpmyadmin-dev
 
+if [ ! -d $VOLUME_PATH ] ; then
+    mkdir $VOLUME_PATH
+fi
+
 # start database, phpmyadmin and backend-dev containers
-docker run -e MYSQL_ROOT_PASSWORD=p4ssw0rd --name database-dev -h database-dev -d --network dev-net mariadb:10.4.3
+docker run -v $VOLUME_PATH:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=p4ssw0rd --name database-dev -h database-dev -d --network dev-net mariadb:10.4.3
 docker run --name phpmyadmin-dev -h phpmyadmin-dev -d -p 8081:80 -e PMA_HOST=database-dev --network dev-net phpmyadmin/phpmyadmin
-docker run --name backend-dev -h backend-dev -d -p 8080:3000 backend-dev
+docker run --name backend-dev -h backend-dev -d -p 3000:3000 backend-dev
