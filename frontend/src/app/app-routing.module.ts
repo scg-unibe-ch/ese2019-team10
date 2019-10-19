c99977884f59749c01f7131b2a487abc1d0ba2d6
BenjaminBlaesi <benjamin.blaesi@bluewin.ch>
2019-10-19 15:03:20 +0200

    dark toolbar

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,23 +1,25 @@
-
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 // import {LoginPage} from './pages/login/login.page';
 // import {RegisterPage} from './pages/register/register.page';
-import { AuthGuardService } from './services/auth-guard.service';
+import {AuthGuardService} from './services/auth-guard.service';
 
 const routes: Routes = [
   // {  path: 'register', component: RegisterPage },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
-  { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
-  { path: 'registered', loadChildren: './pages/registered/registered.module#RegisteredPageModule' },
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', loadChildren: './pages/home/home.module#HomePageModule'},
+  {path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule'},
+  {path: 'registered', loadChildren: './pages/registered/registered.module#RegisteredPageModule'},
   // { path: 'login', component: LoginPage },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'terms', loadChildren: './pages/terms/terms.module#TermsPageModule' },
-  { path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardPageModule', canActivate: [AuthGuardService]
+  {path: 'login', loadChildren: './pages/login/login.module#LoginPageModule'},
+  {path: 'terms', loadChildren: './pages/terms/terms.module#TermsPageModule'},
+  {
+    path: 'dashboard',
+    loadChildren: './pages/dashboard/dashboard.module#DashboardPageModule',
+    canActivate: [AuthGuardService]
   },
-  { path: 'profile', loadChildren: './pages/profile/profile.module#ProfilePageModule' },
-  ];
+  {path: 'profile', loadChildren: './pages/profile/profile.module#ProfilePageModule'},
+];
 
 @NgModule({
   imports: [
@@ -26,4 +28,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
