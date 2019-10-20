946530da76682536f74af5fee941a0ba141fd855
David Herrmann <david.herrmann@protonmail.com>
2019-10-20 21:19:15 +0200

    Add Access-Control-Allow-Origin header entry for options request on /api/login and /api/register

--- backend/app/controllers/register.controller.ts
+++ backend/app/controllers/register.controller.ts
@@ -5,6 +5,11 @@ import {User} from '../models/user.model';
 const router: Router = Router();
 const Op = Sequelize.Op;
 
+router.options('/', (req:  Request, res: Response ) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
+  res.header( 'Access-Control-Allow-Methods', 'GET, POST, PUT');
+  res.sendStatus(200);
+})
 
 router.get('/approved', async (req: Request, res: Response) => {
   let options = {};
