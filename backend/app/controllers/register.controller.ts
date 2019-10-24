3004d16cc196642c9dcd1e513581f8293cecadfe
jairoerazobotero <jairoerazo@hotmail.com>
2019-10-24 21:02:42 +0200

    Error control with promises in the register controller

--- backend/app/controllers/register.controller.ts
+++ backend/app/controllers/register.controller.ts
@@ -9,37 +9,50 @@ router.options('/', (req:  Request, res: Response ) => {
   res.sendStatus(200);
 });
 
+// Function to list approved users
 router.get('/approved', async (req: Request, res: Response) => {
   let options = {};
   options = {
+    attributes: ['id', 'firstName', 'lastName', 'email'],
     where: {
       approved: true
     }
   };
-  const instances = await User.findAll(options);
-  res.statusCode = 200;
-  res.send(instances.map(e => e));
+  User.findAll(options).then(result => {
+    res.statusCode = 200;
+    res.json(result.map(e => e));
+  }).catch(error  => {
+    res.statusCode = 500;
+    res.json({'msg': 'Error, there is not user list'});
+  });
 });
 
+// Function to list users to approve
 router.get('/to-approve', async (req: Request, res: Response) => {
   let options = {};
   options = {
-      where: {
-        approved: false
-      }
+    attributes: ['id', 'firstName', 'lastName', 'email'],
+    where: {
+      approved: false
+    }
   };
-  const instances = await User.findAll(options);
-  res.statusCode = 200;
-  res.send(instances.map(e => e));
+  User.findAll(options).then(result => {
+    res.statusCode = 200;
+    res.json(result.map(e => e));
+  }).catch(error  => {
+    res.statusCode = 500;
+    res.json({'msg': 'Error, there is not user list'});
+  });
 });
 
+// Function to approve users
 router.put('/approve/:id', async (req: Request, res: Response) => {
   const id = parseInt(req.params.id, undefined);
   const instance = await User.findByPk(id);
   if (instance == null) {
     res.statusCode = 404;
     res.json({
-      'msg': 'not found'
+      'msg': 'Not found'
     });
     return;
   }
@@ -49,17 +62,26 @@ router.put('/approve/:id', async (req: Request, res: Response) => {
     where: {
       id: id
     }
+  }).then(result => {
+    res.statusCode = 200;
+    res.json({'msg': 'User approved'});
+  }).catch(error  => {
+    res.statusCode = 500;
+    res.json({'msg': 'Error, user not approved'});
   });
-  res.statusCode = 200;
-  res.send('{"msg": "User approved"}');
 });
 
+// Function to create users
 router.post('/', async (req: Request, res: Response) => {
   const instance = new User();
   instance.post_(req.body);
-  await instance.save();
-  res.statusCode = 201;
-  res.send('{"msg": "User created"}');
+  instance.save().then(result => {
+    res.statusCode = 201;
+    res.json({'msg': 'User created'});
+  }).catch(error  => {
+    res.statusCode = 500;
+    res.json({'msg': 'Error, user not created'});
+  });
 });
 
 
