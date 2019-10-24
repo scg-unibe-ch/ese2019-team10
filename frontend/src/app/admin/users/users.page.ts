8d7609b9d95dcd595e67e469cdd6aff3298427d5
BenjaminBlaesi <benjamin.blaesi@bluewin.ch>
2019-10-24 17:39:44 +0200

    users admin page functionality and layout

--- frontend/src/app/admin/users/users.page.ts
+++ frontend/src/app/admin/users/users.page.ts
@@ -10,7 +10,10 @@ import {AlertService} from '../../services/alert.service';
 })
 export class UsersPage implements OnInit {
   private approvedUsers: any;
+  private numberApproved: number;
   private unapprovedUsers: any;
+  private numberUnapproved: number;
+  private data: any;
 
   constructor(
     private authService: AuthService,
@@ -19,23 +22,28 @@ export class UsersPage implements OnInit {
   )  { }
 
   ngOnInit() {
+  this.numberApproved = -1;
+  this.numberUnapproved = -1;
   }
 
   loadApprovedUsers() {
     this.authService.getApprovedUsers().subscribe((res: any) => {
-      this.approvedUsers = res.msg;
+      this.approvedUsers = res;
+      this.numberApproved = res.length;
+      console.log(res);
     });
   }
 
   loadUnapprovedUsers() {
     this.authService.getUnapprovedUsers().subscribe((res: any) => {
-      this.unapprovedUsers = res.msg;
-      console.log(res.msg);
-      this.alertService.presentToast(res.msg).then(r => {
+      this.unapprovedUsers = res;
+      this.numberUnapproved = res.length;
+      console.log(res);
+/*      this.alertService.presentToast(res).then(r => {
         console.log(r);
       }, err => {
         console.log(err);
-      });
+      });*/
     });
   }
 
