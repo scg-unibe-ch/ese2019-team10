2a145dbb98852856152e574429e6bb213ca7b377
David Herrmann <david.herrmann@students.unibe.ch>
2019-11-06 13:29:37 +0100

    Prevent multiple accounts with same email in register.controller.ts

--- backend/app/controllers/register.controller.ts
+++ backend/app/controllers/register.controller.ts
@@ -5,17 +5,23 @@ const router: Router = Router();
 
 // Function to create users
 router.post('/', async (req: Request, res: Response) => {
-  const instance = new User();
-  instance.post_(req.body);
-  instance.save().then(result => {
-    res.statusCode = 201;
-    res.json({'msg': 'User created'});
-  }).catch(error  => {
-    res.statusCode = 500;
-    res.json({'msg': 'Error, user not created'});
+  // search for user with requested email. if it already exists (result is not null), return 400
+  User.findOne({where: {'email': req.body['email']}}).then(user => {
+    if (user === null) {
+      const instance = new User();
+      instance.post_(req.body);
+      instance.save().then(result => {
+        res.statusCode = 201;
+        res.json({'msg': 'User created'});
+      }).catch(error  => {
+        res.statusCode = 500;
+        res.json({'msg': 'Error, user not created'});
+      });
+    } else {
+      res.statusCode = 400;
+      res.json({'msg': 'E-mail address already in use'});
+    }
   });
 });
-
-
 
 export const RegisterController: Router = router;
