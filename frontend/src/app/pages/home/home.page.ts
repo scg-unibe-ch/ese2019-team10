9b0478ff59d983daf2950ba2b9fcb951c20e41d1
Benjamin Hodel <benj.hodel@gmail.com>
2019-11-13 09:50:51 +0100

    Added color schemes

--- frontend/src/app/pages/home/home.page.ts
+++ frontend/src/app/pages/home/home.page.ts
@@ -1,5 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import {Component, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {ThemeService} from '../../theme.service';
+
+const themes = {
+  autumn: {
+    primary: '#F78154',
+    secondary: '#4D9078',
+    tertiary: '#B4436C',
+    light: '#FDE8DF',
+    medium: '#FCD0A2',
+    dark: '#B89876'
+  },
+  night: {
+    primary: '#8CBA80',
+    secondary: '#FCFF6C',
+    tertiary: '#FE5F55',
+    medium: '#BCC2C7',
+    dark: '#F7F7FF',
+    light: '#495867'
+  },
+  neon: {
+    primary: '#39BFBD',
+    secondary: '#4CE0B3',
+    tertiary: '#FF5E79',
+    light: '#F4EDF2',
+    medium: '#B682A5',
+    dark: '#822a78'
+  }
+};
 
 @Component({
   selector: 'app-home',
@@ -11,7 +39,12 @@ export class HomePage implements OnInit {
 
   constructor(
     private titleService: Title,
+    private theme: ThemeService
   ) { }
+
+  changeTheme(name) {
+    this.theme.setTheme(themes[name]);
+  }
 
   ngOnInit() {
     this.title = 'Home';
@@ -19,3 +52,8 @@ export class HomePage implements OnInit {
   }
 
 }
+
+
+
+
+
