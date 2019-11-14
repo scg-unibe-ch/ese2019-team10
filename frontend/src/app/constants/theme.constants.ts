12f8243bbfe68ea274dd131688302a4d0c290887
BenjaminBlaesi <benjamin.blaesi@bluewin.ch>
2019-11-14 15:30:13 +0100

    put themes into constants file

---
+++ frontend/src/app/constants/theme.constants.ts
@@ -0,0 +1,28 @@
+export const themeConstants = {
+  autumn: {
+    primary: '#F78154',
+    secondary: '#4D9078',
+    tertiary: '#B4436C',
+    light: '#FDE8DF',
+    medium: '#FCD0A2',
+    dark: '#B89876'
+  },
+  night: {
+    primary: '#8CBA80',
+    secondary: '#FCFF6C',
+    tertiary: '#FE5F55',
+    medium: '#BCC2C7',
+    dark: '#F7F7FF',
+    light: '#495867'
+  },
+  neon: {
+    primary: '#39BFBD',
+    secondary: '#4CE0B3',
+    tertiary: '#FF5E79',
+    light: '#F4EDF2',
+    medium: '#B682A5',
+    dark: '#822a78'
+  }
+};
+
+
--- frontend/src/app/pages/home/home.page.ts
+++ frontend/src/app/pages/home/home.page.ts
@@ -3,34 +3,7 @@ import {Title} from '@angular/platform-browser';
 
 import {ThemeService} from '../../theme.service';
 import {appConstants} from '../../constants/app.constants';
-
-
-const themes = {
-  autumn: {
-    primary: '#F78154',
-    secondary: '#4D9078',
-    tertiary: '#B4436C',
-    light: '#FDE8DF',
-    medium: '#FCD0A2',
-    dark: '#B89876'
-  },
-  night: {
-    primary: '#8CBA80',
-    secondary: '#FCFF6C',
-    tertiary: '#FE5F55',
-    medium: '#BCC2C7',
-    dark: '#F7F7FF',
-    light: '#495867'
-  },
-  neon: {
-    primary: '#39BFBD',
-    secondary: '#4CE0B3',
-    tertiary: '#FF5E79',
-    light: '#F4EDF2',
-    medium: '#B682A5',
-    dark: '#822a78'
-  }
-};
+import {themeConstants} from '../../constants/theme.constants';
 
 @Component({
   selector: 'app-home',
@@ -44,15 +17,16 @@ export class HomePage implements OnInit {
   constructor(
     private titleService: Title,
     private theme: ThemeService,
-  ) { }
+  ) {
+  }
 
   changeTheme(name) {
-    this.theme.setTheme(themes[name]);
+    this.theme.setTheme(themeConstants[name]);
   }
 
   ngOnInit() {
     this.title = 'Home';
-    this.titleService.setTitle (this.title + appConstants.APPENDED_TITLE);
+    this.titleService.setTitle(this.title + appConstants.APPENDED_TITLE);
   }
 
 }
