0d62eae02502915805b8fb922d9be6dd38df00f9
BenjaminBlaesi <benjamin.blaesi@bluewin.ch>
2019-11-21 20:44:12 +0100

    display search results

--- frontend/src/app/pages/search/search.page.ts
+++ frontend/src/app/pages/search/search.page.ts
@@ -3,6 +3,9 @@ import {Title} from '@angular/platform-browser';
 
 import {appConstants} from '../../constants/app.constants';
 import {AuthService} from '../../services/auth.service';
+import {Service} from '../../models/service.model';
+import {Event} from '../../models/event.model';
+import {User} from '../../models/user.model';
 
 @Component({
   selector: 'app-search',
@@ -17,6 +20,11 @@ export class SearchPage implements OnInit {
   foundServices: boolean;
   foundEvents: boolean;
   searchCategory: any;
+  services: Service[];
+  events: Event[];
+  users: User[];
+
+  searched: boolean;
 
   constructor(
     private titleService: Title,
@@ -37,7 +45,6 @@ export class SearchPage implements OnInit {
 
   search(term) {
 
-    document.getElementById('results').textContent = term;
 
     if (term.length > 0) {
 
@@ -47,10 +54,27 @@ export class SearchPage implements OnInit {
       };
 
       console.log(searchObject);
+      this.searched = true;
 
       this.authService.search(searchObject).subscribe((result: any) => {
         this.searchResult = result;
-        this.foundResult = true;
+
+        if (this.searchResult.services.length > 0) {
+          this.foundServices = true;
+          this.services = this.searchResult.services;
+        }
+        if (this.searchResult.events.length > 0) {
+          this.foundEvents = true;
+          this.events = this.searchResult.events;
+        }
+        if (this.searchResult.users.length > 0) {
+          this.foundUsers = true;
+          this.users = this.searchResult.users;
+        }
+        if (this.foundServices || this.foundEvents || this.foundUsers) {
+          this.foundResult = true;
+        }
+
         console.log(result);
       });
 
