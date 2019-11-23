87532e1ed920a8ca9d861149dacebce00d9f80de
BenjaminBlaesi <benjamin.blaesi@bluewin.ch>
2019-11-23 14:02:48 +0100

    terms and conditions

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   {path: 'login', loadChildren: './pages/login/login.module#LoginPageModule'},
   {path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule'},
   {path: 'registered', loadChildren: './pages/registered/registered.module#RegisteredPageModule'},
-  {path: 'terms', loadChildren: './pages/terms/terms.module#TermsPageModule'},
+  {path: 'terms-conditions', loadChildren: './pages/terms/terms.module#TermsPageModule'},
   {path: 'search', loadChildren: './pages/search/search.module#SearchPageModule'},
   {
     path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardPageModule',
