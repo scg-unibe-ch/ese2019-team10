bc2d51383f807ad5b8ab11b4fc7190ca1737b6d0
BenjaminBlaesi <benjamin.blaesi@bluewin.ch>
2019-12-04 18:44:33 +0100

    shorter, clearer validation messages

--- frontend/src/app/constants/event-validation.constants.ts
+++ frontend/src/app/constants/event-validation.constants.ts
@@ -1,18 +1,18 @@
 export const EventValidation = {
   name: [
-    {type: 'required', message: 'Your event should have a name.'},
-    {type: 'maxlength', message: 'Your event\'s name should be less than 100 characters long.'},
+    {type: 'required', message: 'Name is required.'},
+    {type: 'maxlength', message: 'Name must be shorter than 100 characters.'},
   ],
   place: [
-    {type: 'required', message: 'Your event should have a place.'},
-    {type: 'maxlength', message: 'Your event\'s place should be less than 100 characters long.'},
+    {type: 'required', message: 'Place is required.'},
+    {type: 'maxlength', message: 'Place must be shorter than 100 characters.'},
   ],
   date: [
-    {type: 'required', message: 'Your event should have a date.'},
-    {type: 'maxlength', message: 'Your event\'s date should be less than 100 characters long.'},
+    {type: 'required', message: 'Date is required.'},
+    {type: 'maxlength', message: 'Date must be shorter than 100 characters.'},
   ],
   description: [
-    {type: 'required', message: 'Your event should have a description.'},
-    {type: 'maxlength', message: 'Your event\'s description should be less than 1000 characters long.'},
+    {type: 'required', message: 'Description is required.'},
+    {type: 'maxlength', message: 'Description must be shorter than 1000 characters.'},
   ],
 };
