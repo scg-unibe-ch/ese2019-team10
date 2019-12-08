451433389e9b2b8abd6a5d07218ed4bcebe09357
BenjaminBlaesi <benjamin.blaesi@bluewin.ch>
2019-12-08 17:30:30 +0100

    update tests

--- frontend/src/app/components/header/header.component.spec.ts
+++ frontend/src/app/components/header/header.component.spec.ts
@@ -1,18 +1,36 @@
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import {HeaderComponent} from './header.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {IonicStorageModule} from '@ionic/storage';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let jwtHelperSpy;
+  let jwtHelper: JwtHelperService;
 
   beforeEach(async(() => {
+    jwtHelperSpy = jasmine.createSpyObj('JwtHelperService', ['hide']);
+
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ],
+      declarations: [HeaderComponent],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        IonicStorageModule.forRoot(),
+      ],
+      providers: [
+        {provide: JwtHelperService, useValue: jwtHelperSpy},
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
-    .compileComponents();
+      .compileComponents();
+    jwtHelper = TestBed.get(JwtHelperService);
+
   }));
 
   beforeEach(() => {
